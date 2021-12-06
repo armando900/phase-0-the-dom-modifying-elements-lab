f4a4bbba49a08022a528a970885f8119d83b34d4
Armando <seniorbuso@gmail.com>
2021-12-06 14:51:41 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.setAttribute("h1", "vitory");
+
+newHeader.textContent = "YOUR-NAME is the champion";
